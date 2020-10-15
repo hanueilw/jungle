ed20e21a0f220ac5202598563652d5a66f9e1cfd
hanueilw <pmy3581040@naver.com>
2020-10-15 21:27:01 +0900

    1015

--- class_8/js/jquery_ui.js
+++ class_8/js/jquery_ui.js
@@ -9,9 +9,27 @@ $(function(){
 
 
 	//메인 베너 슬라이드
-	  
+	  $(".section.visual > ul").bxSlider({
+		  sliderWidth:1000,
+		  slideMargin:209,
+		  minSliders:1,
+		  maxSlides:2,
+		  moveSlides:1,
+		  onSliderLoad:function(){
+			  //처음 페이지가 실행될때 
+			slide_resize();
+		  }
+	  });
 
-	//베스트 아이템 슬라이드
-		
+	  $(window).resize(function(){
+		slide_resize();
+	  })
 
+	  function slide_resize(){
+		var main_slide_w = ($(".visual").width() - $(".visual .bx-wrapper ul >li").width())/2;
+		console.log(main_slide_w)
+		$(".visual .bx-wrapper ul").css({"margin-left":main_slide_w});
+	}
+
+	//베스트 아이템 슬라이드 
 })
